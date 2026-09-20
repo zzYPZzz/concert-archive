c6b0ddf743cfbb66b0c21efcd4381a39c06fba36
zzYPZzz <pyang940@gmail.com>
2026-09-20 22:33:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log("My Concert Database 已啟動！");
+function openConcertForm() {
+
+    const form = document.getElementById("concertForm");
+
+    form.style.display = "flex";
+
+}
+
+
+function closeConcertForm() {
+
+    const form = document.getElementById("concertForm");
+
+    form.style.display = "none";
+
+}
+
+
+function saveConcert() {
+
+    alert("演唱會資料已送出！");
+
+}function openConcertForm() {
+
+    const form = document.getElementById("concertForm");
+
+    form.style.display = "flex";
+
+}
+
+
+function closeConcertForm() {
+
+    const form = document.getElementById("concertForm");
+
+    form.style.display = "none";
+
+}
+
+
+function saveConcert() {
+
+    alert("演唱會資料已送出！");
+
+}
